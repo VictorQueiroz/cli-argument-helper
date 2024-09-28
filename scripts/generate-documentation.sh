0011c2e1a3598aa2a916a965feaa4f9bae140198
Victor Queiroz <victorcqueirozg@gmail.com>
2024-09-28 17:47:18 -0300

    Update generated HTML

---
+++ scripts/generate-documentation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+package_name=$(jq -r ".name" <<< cat "./package.json")
+
+typedoc_output_directory="docs"
+
+typedoc_arguments=(
+  "--searchInComments"
+  "--darkHighlightTheme" "dracula"
+  "--githubPages"
+  "--out" "$typedoc_output_directory"
+  "--tsconfig" "./tsconfig.json"
+  "--entryPoints" "./**/*.ts"
+  "--exclude" "node_modules"
+  "--exclude" "./**/*.d.ts"
+  "--name" "$package_name"
+  "--hostedBaseUrl" "https://victorqueiroz.github.io/$package_name"
+)
+
+# Generate the documentation
+npx typedoc "${typedoc_arguments[@]}"
+
+# Move the generated documentation to the root directory
+rsync --remove-source-files -avz --progress -r ${typedoc_output_directory}/ .
+
+# Remove empty directories
+find . -type d -empty -delete
